122e325be02af9a2176ee4d7bb22986d9757b927
calvin4370 <calvin.j.j.chan@gmail.com>
2025-12-03 01:13:18 +0800

    feat: Add geocoding script to fetch HDB coordinates from OneMap API
    - Added `scripts/03_geocoding.R` for calling OneMap API to retrieve coordinates for all addresses.
    - Added error handling (tryCatch) and rate limiting to ensure stable API requests.
    - 03_geocoding.R script generates `data/hdb_coordinates.csv` for geospatial feature engineering for linear regression model later.

--- scripts/02_eda.R
+++ scripts/02_eda.R
@@ -9,7 +9,22 @@ ggplot(data = df, aes(x = floor_area_sqm, y = resale_price)) +
   geom_smooth(method = "lm", color = "red") +
   theme_minimal() +
   labs(
-    title = "Resale Price vs. Floor Area",
+    title = "Resale Price vs. Floor Area (sqm)",
     x = "Floor Area (sqm)",
     y = "Resale Price ($)"
+  )
+
+
+# Boxplot of Resale Price against Town
+# Sort by average resale price descending
+ggplot(data = df, aes(y = reorder(town, resale_price), x = resale_price)) +
+  geom_boxplot(fill = "lightblue", color = "darkblue", alpha = 0.7) +
+  
+  theme_minimal() +
+  
+  labs(
+    title = "Resale Price Distribution by Town",
+    subtitle = "Central locations tend to have higher resale prices",
+    x = "Resale Price ($)",
+    y = "Town"
   )
