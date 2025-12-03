f937fd7a44a80391cfacd3aa39dc00febaefc0af
calvin4370 <calvin.j.j.chan@gmail.com>
2025-12-03 21:57:02 +0800

    feat: Engineer distance-to-nearest-mrt feature using geospatial data
    - Added mrt_lrt_stations.csv to collect coord data of mrt and lrt stations
    - Updated 05_feature_engineering.R to engineer distance-to-nearest-mrt feature
    - Updated README.md to include references for raw data sources

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hdb-resale-analytics
 A valuation model to predict HDB resale prices using linear regression
+
+## 📁 Data Sources
+
+1.  **HDB Resale Prices: raw_resale_prices.csv**
+    * *Source:* [Data.gov.sg](https://data.gov.sg/)
+    * *Dataset:* Resale flat prices based on registration date from Jan-2017 onwards.
+
+2.  **Coordinates of HDB addreses: hdb_coordinates.csv**
+    * *Source:* [OneMap API](https://www.onemap.gov.sg/docs/)
+    * Generated csv by calling the OneMap API to obtain coordinate (lat / long) info of all HDB addresses
+
+3.  **MRT Station Locations: mrt_lrt_stations.csv**
+    * *Source:* [Kaggle - MRT & LRT Stations in Singapore](https://www.kaggle.com/datasets/lzytim/full-list-of-mrt-and-lrt-stations-in-singapore)
--- scripts/05_feature_engineering.R
+++ scripts/05_feature_engineering.R
@@ -1,17 +1,26 @@
 # ==============================================================================
 # SCRIPT: 05_feature_engineering.R
 # PURPOSE: Engineers the features: `distance_to_cbd` and `distance_to_nearest_mrt`
+# DATA LINEAGE:
+# 1. 'cleaned_resale_prices.csv' -> Derived from Data.gov.sg raw files
+# 2. 'hdb_coordinates.csv' -> created from calling OneMap API to obtain coords of all hdb addresses
 # ==============================================================================
 
 library(tidyverse)
 
 source("scripts/00_utils.R") # Import haversine_km function
 
+
+# -------------------- Load data -------------------- #
 resale_prices <- read_csv("data/cleaned_resale_prices.csv")
 hdb_address_coords <- read_csv("data/hdb_coordinates.csv")
 
+mrt_coords <- read_csv("data/mrt_lrt_stations.csv") %>%
+  rename(station_name = STATION_NAME_ENGLISH, lat = LATITUDE, long = LONGITUDE) %>%
+  select(station_name, lat, long)
 
-# -------------------- Outer join (left) the two dfs -------------------- #
+
+# ---------- Outer join (left) the resale_prices and hdb_address_coords dfs ---------- #
 # Add a common key "address" to prices df to join prices and hdb_address_coords
 prices_with_address <- resale_prices %>%
   mutate(address = paste(block, street_name))
@@ -40,7 +49,24 @@ summary(enriched_data$distance_to_cbd)
 
 
 # 2. Add `distance_from_nearest_mrt` to complete enriched data
-# TODO
+# Use vectorization to computer minimum distance from any mrt 
+# (distance_from_nearest_mrt) for every address at the same time
+enriched_data$distance_to_nearest_mrt <- 99999 # initialise as a large number
+
+# Loop through each MRT station to find the minimum distance from any mrt for every hdb address at once
+for (i in 1 : nrow(mrt_coords)) {
+  curr_mrt_lat <- mrt_coords$lat[i]
+  curr_mrt_long <- mrt_coords$long[i]
+  
+  dist_to_curr_mrt <- haversine_km(enriched_data$lat, enriched_data$long, curr_mrt_lat, curr_mrt_long)
+  
+  # Relax the entire distance_to_nearest_mrt column with min of its current value and dist_to_curr_mrt
+  enriched_data$distance_to_nearest_mrt <- pmin(enriched_data$distance_to_nearest_mrt, 
+                                                dist_to_curr_mrt, na.rm = TRUE)
+}
+
+# Check summary of distance_to_nearest_mrt to visually ensure nothing went wrong
+summary(enriched_data$distance_to_nearest_mrt)
 
 
 # ---------- Save the enriched data (with new features) to a new csv ---------- #
